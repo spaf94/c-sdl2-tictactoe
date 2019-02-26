dc0e777c4e6162e1e3d9a82e87e2404d712b50dd
Simão Ferreira <spaf94@outlook.pt>
2019-02-26 20:48:56 +0000

    Restarted game devolepment, new structure.

--- c-sdl2-tiktaktoe/src/main.c
+++ c-sdl2-tiktaktoe/src/main.c
@@ -1,7 +1,6 @@
 /******************************************************************************/
 
-#include "tictactoe_ctx.h"
-#include "tictactoe_events.h"
+#include "internal.h"
 
 /******************************************************************************/
 
@@ -9,21 +8,7 @@ int main()
 {
     // Init SDL (takes some time)
     SDL_Init( SDL_INIT_VIDEO );
-    // Create game context
-    tictactoe_ctx_t *ctx = tictactoe_ctx_new();
-    if ( ctx == NULL )
-        goto end;
 
-    while ( ctx->running )
-    {
-        tictactoe_events_process( ctx );
-        tictactoe_ctx_renderize( ctx );
-    }
-
-end:
-    // Release game context
-    tictactoe_ctx_free( ctx );
-    // Release SDL
     SDL_Quit();
 
     return 0;
